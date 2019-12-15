b01ade32edbbb21509e7bb75ad463c18c5302a23
Fábio Muniz Developer <fabio.muniz01@gmail.com>
2019-12-14 21:39:56 -0300

    exlusao usuario

--- Infraero.Relprev.Application/Usuarios/Queries/GetUsuarios/PriorityLevelDto.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace Infraero.Relprev.Application.Usuarios.Queries.GetUsuarios
-{
-    public class PriorityLevelDto
-    {
-        public int Value { get; set; }
-
-        public string Name { get; set; }
-    }
-}
