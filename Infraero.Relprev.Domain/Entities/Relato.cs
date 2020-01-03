d3e7dbe040f77e35a7d2556b42b97689d9282c7e
Eduardo Fernandes <eduardofernandes@wizsolucoes.com.br>
2020-01-03 15:26:11 -0300

    Relato

---
+++ Infraero.Relprev.Domain/Entities/Relato.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Text;
+
+namespace Infraero.Relprev.Domain.Entities
+{
+    public class Relato
+    {
+        [Key]
+        public int CodRelato { get; set; }
+        public DateTime DatOcorrencia { get; set; }
+        public TimeSpan HorOcorrencia { get; set; }
+        public string DscLocalOcorrenciaRelator { get; set; }
+        public string DscEnvolvidosOcorrencia { get; set; }
+        public string DscOcorrenciaRelator { get; set; }
+        public string NomRelator { get; set; }
+        public string EmailRelator { get; set; }
+        public string NumTelefoneRelator { get; set; }
+        public string NomEmpresaRelator { get; set; }
+
+        [ForeignKey("CodRelato")]
+        public virtual ICollection<RelatoArquivo> Arquivos { get; set; }
+    }
+}
