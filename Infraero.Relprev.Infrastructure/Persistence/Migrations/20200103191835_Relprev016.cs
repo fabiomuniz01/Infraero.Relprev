a0cdf5e7a01b18641affc001ee400f2f3d697159
Fábio Muniz Developer <fabio.muniz01@gmail.com>
2020-01-03 16:21:58 -0300

    migrations alteracao tipo relato

---
+++ Infraero.Relprev.Infrastructure/Persistence/Migrations/20200103191835_Relprev016.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Infraero.Relprev.Infrastructure.Persistence.Migrations
+{
+    public partial class Relprev016 : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<string>(
+                name: "HorOcorrencia",
+                table: "Relato",
+                nullable: true,
+                oldClrType: typeof(TimeSpan),
+                oldType: "time");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AlterColumn<TimeSpan>(
+                name: "HorOcorrencia",
+                table: "Relato",
+                type: "time",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldNullable: true);
+        }
+    }
+}
