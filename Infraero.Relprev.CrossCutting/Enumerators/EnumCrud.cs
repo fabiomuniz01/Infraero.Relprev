755fc669333622186ed7c34cd35d9c002ed1d491
Developer <ntitsolutions01@gmail.com>
2020-05-30 18:49:02 -0300

    enumerador + extension method para pegar descrição do enum

--- Infraero.Relprev.CrossCutting/Enumerators/EnumCrud.cs
+++ Infraero.Relprev.CrossCutting/Enumerators/EnumCrud.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Text;
-
-namespace Infraero.Relprev.CrossCutting.Enumerators
+﻿namespace Infraero.Relprev.CrossCutting.Enumerators
 {
     public enum EnumCrud
     {
